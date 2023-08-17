75b3ec658882280f10c52f904b39503102d7c516
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2023-08-17 17:07:51 +0530

    version_1.5

--- components/info.tsx
+++ components/info.tsx
@@ -2,11 +2,18 @@ import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "./ui/button";
 import { ShoppingCartIcon } from "lucide-react";
+import { MouseEventHandler } from "react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 const Info: React.FC<InfoProps> = ({ data }) => {
+ const cart = useCart();
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event)=>{
+    event.stopPropagation();
+    cart.addItem(data)
+  }
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>

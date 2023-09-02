cce2e6c05f76b3012342f356269f873ee47a64eb
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2023-09-02 13:08:13 +0530

    Version_2.1

--- components/footer.tsx
+++ components/footer.tsx
@@ -17,13 +17,13 @@ const Footer = () => {
           <a href="https://wa.me/+919354227521?text=I%20want%20to%20know%20more%20about%20healthcare%20services%20offered%20by%20DawaDuniya.%20"><FontAwesomeIcon size="2x" color="white" icon={faWhatsappSquare}/></a>
           <a href="https://youtube.com/@Dawa_Duniya?si=_lKGeYe8E2TAuifR"><FontAwesomeIcon size="2x" color="white" icon={faYoutubeSquare}/></a>
           <a href="https://www.facebook.com/profile.php?id=61550863916779&mibextid=ZbWKwL"><FontAwesomeIcon size="2x" color="white" icon={faSquareFacebook}/></a>
-          <a href=""><Mails color="white" size={40}/></a>
+          <a href="mailto:info@dawaduniya.com"><Mails color="white" size={40}/></a>
         </div>
       <div className="flex space-x-2 text-white mt-6">
         <div className="social border-r border-white md:w-3/4">
             <h1 className="text-xl font-bold text-center w-full">  <InfoIcon  className="inline"/> About Us</h1>
             <p className="px-3 items-center">
-            Dawa Duniya is a group of youngsters coming from various professions such as: pharmacists, and doctors who have joined forces to serve mankind. Our shared goal is to provide access to quality healthcare and education. By combining our skills, we aim to make a greater impact than working alone. Our team is a shining example of the positive change that can come from collaboration and teamwork.
+            Dawa Duniya is a group of youngsters coming from various professions such as: pharmacists, and doctors who have joined forces to serve mankind. Our shared goal is to provide access to quality healthcare. By combining our skills, we aim to make a greater impact than working alone. Our team is a shining example of the positive change that can come from collaboration and teamwork.
             </p>
         </div>
         <div className="pb-6 md:w-1/4">
